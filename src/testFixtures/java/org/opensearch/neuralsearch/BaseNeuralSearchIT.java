2e7101bfce899f03e5a38c1e10ab7f197ba035a4
Martin Gaievski <gaievski@amazon.com>
2024-01-29 16:22:32 -0800

    Support w/wo parallel segment search, add more tests
    Signed-off-by: Martin Gaievski <gaievski@amazon.com>

--- src/testFixtures/java/org/opensearch/neuralsearch/BaseNeuralSearchIT.java
+++ src/testFixtures/java/org/opensearch/neuralsearch/BaseNeuralSearchIT.java
@@ -115,6 +115,9 @@ public abstract class BaseNeuralSearchIT extends OpenSearchSecureRestTestCase {
         // default threshold for native circuit breaker is 90, it may be not enough on test runner machine
         updateClusterSettings("plugins.ml_commons.native_memory_threshold", 100);
         updateClusterSettings("plugins.ml_commons.allow_registering_model_via_url", true);
+        // this way segment search will be disabled by default, we need to enable it via feature flag first as otherwise it's not possible
+        // to turn it on/off via cluster setting
+        updateClusterSettings("search.concurrent_segment_search.enabled", false);
     }
 
     @SneakyThrows
@@ -819,6 +822,9 @@ public abstract class BaseNeuralSearchIT extends OpenSearchSecureRestTestCase {
 
         for (String dateField : dateFields) {
             xContentBuilder.startObject(dateField).field("type", "date").field("format", "MM/dd/yyyy").endObject();
+
+        for (String keywordField : dateFields) {
+            xContentBuilder.startObject(keywordField).field("type", "date").field("format", "MM/dd/yyyy").endObject();
         }
 
         xContentBuilder.endObject().endObject().endObject();
